42316f645b5d8c917dc4c2748963aa98f6d77ad3
R3N0N-0000 <117981827+R3N0N-0000@users.noreply.github.com>
2022-11-20 13:25:55 +0000

    :(

---
+++ ブラックジャック.py
@@ -0,0 +1,19 @@
+while :
+
+    # カードを配る
+
+    while STANDを選ぶまで:
+        # HITかSTANDを選ばせる
+        
+        # HITならカードを1枚配る
+
+    # CPU
+
+    if player == cpu or (player > 21 and cpu > 21):
+        # EVEN
+    elif player == 21 and cpu != 21:
+        # BlackJack
+    elif player > cpu and player <= 21 and cpu <= 21 :
+        # WIN
+    else:
+        # LOSE
