ff807bb59ed7936b165b27402110c64b642cecc3
R3N0N-0000 <117981827+R3N0N-0000@users.noreply.github.com>
2022-11-20 11:14:56 +0000

    :(

--- いいい.py
+++ いいい.py
@@ -3,20 +3,20 @@ import random
 
 
 # 乱数2つ
-player = random.randint(1, 6)
-cpu = random.randint(1, 6)
-print("You :", player)
-
-
-# 予想させる
-predict = input("higher or lower than CPU")
-
-
-# 点数をつける
 score = 0
-print("cpu :",cpu)
-if (predict == "higher" and player > cpu) or (predict == "lower" and player < cpu):
-    score += 1
+for _ in range(3):
+    player = random.randint(1, 6)
+    cpu = random.randint(1, 6)
+    print("You :", player)
 
+    # 予想させる
+    predict = input("higher or lower than CPU ")
 
-# 指定回数で終わらせる  
+    # 点数をつける
+    print("cpu :",cpu)
+    if (predict == "higher" and player > cpu) or (predict == "lower" and player < cpu):
+        score += 1
+    print("score :",score)
+
+    # 指定回数で終わらせる
+
