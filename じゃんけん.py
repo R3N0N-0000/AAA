dcad856237d6441cb547c1151c4367f81da6f0a3
R3N0N-0000 <117981827+R3N0N-0000@users.noreply.github.com>
2022-11-23 08:37:00 +0000

    :(

--- じゃんけん.py
+++ じゃんけん.py
@@ -1,9 +1,23 @@
 import random
 
-HANDS = ("✊","✌","✋")
+
 cpu = random.randint(1,3)
-player = input()
+player = int(input("グー：１、チョキ：２、パー：３　あなたが出す手は？"))
 
 
-if 
-
+if player == cpu:
+    print("あいこです。")
+elif player == 1 and cpu == 2:
+    print("相手はチョキを出しました。あなたの勝ちです。")
+elif player == 1 and cpu == 3:
+    print("相手はパーを出しました。あなたの負けです。")
+elif player == 2 and cpu == 3:
+    print("相手はパーを出しました。あなたの勝ちです。")
+elif player == 2 and cpu == 1:
+    print("相手はグーを出しました。あなたの負けです。")
+elif player == 3 and cpu == 1:
+    print("相手はグーを出しました。あなたの勝ちです。")
+elif player == 3 and cpu == 2:
+    print("相手はチョキを出しました。あなたの負けです。")
+else:
+    print("1~3の数字を入力してください。")
