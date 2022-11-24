e3a71b379c27aa616cb7fa642e323031eddf127e
R3N0N-0000 <117981827+R3N0N-0000@users.noreply.github.com>
2022-11-24 12:15:53 +0000

    :(

--- じゃんけん.py
+++ じゃんけん.py
@@ -2,14 +2,17 @@ import random
 
 
 cpu = random.randint(1,3)
-player = int(input("1:グー、2:チョキ、3:パー"))
-print("CPU : ",cpu)
+while :
+    player = int(input("1:グー、2:チョキ、3:パー"))
 
-if player == cpu:
-    print("EVEN")
-elif (player == 1 and cpu == 2) or (player == 2 and cpu == 3) or (player == 3 and cpu == 1):
-    print("WIN")
-elif (player == 1 and cpu == 3) or (player == 2 and cpu == 1) or (player == 3 and cpu == 2):
-    print("LOSE")
-else:
-    print("1~3の数字を入力してください。")
+    if player == cpu:
+        print("CPU : ",cpu)
+        print("EVEN")
+    elif (player == 1 and cpu == 2) or (player == 2 and cpu == 3) or (player == 3 and cpu == 1):
+        print("CPU : ",cpu)
+        print("WIN")
+    elif (player == 1 and cpu == 3) or (player == 2 and cpu == 1) or (player == 3 and cpu == 2):
+        print("CPU : ",cpu)
+        print("LOSE")
+    else:
+        print("1~3の数字を入力してください。")
