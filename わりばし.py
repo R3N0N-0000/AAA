462b29dd53284a8d6642b0be9e443b5d9da32d80
R3N0N-0000 <117981827+R3N0N-0000@users.noreply.github.com>
2022-12-09 11:08:13 +0000

    aaaa

--- わりばし.py
+++ わりばし.py
@@ -1,4 +1,9 @@
-right_1, left_1, right_2, left_2 = 1, 1, 1, 1
-
-print(f"{right_2} {left_2}\n\n{left_1} {right_1}")
-
+class UI:
+    right_1, left_1, right_2, left_2 = 1, 1, 1, 1
+    turn = 1
+    def show(self):
+        if self.turn == 1:
+            print(f"{right_2} {left_2}\n\n{left_1} {right_1}")
+        else:
+            print(f"{right_1} {left_1}\n\n{left_2} {right_2}")
+show
