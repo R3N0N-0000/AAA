0156c2658e07208e1f853b8b47901b8d101cc032
R3N0N-0000 <117981827+R3N0N-0000@users.noreply.github.com>
2022-12-11 07:27:42 +0000

    :(

---
+++ README.md
@@ -0,0 +1,1 @@
+https://prod.liveshare.vsengsaas.visualstudio.com/join?FBEDDB1F8D3586F3A7D7B3CDB8AD1779F3A6
--- わりばし.py
+++ わりばし.py
@@ -6,7 +6,7 @@ class Waribashi:
         if self.turn == 1:
             print(f"{self.player_2[1]} {self.player_2[0]}\n\n{self.player_1[0]} {self.player_1[1]}")
         else:
-            print(f"{self.player_1[0]} {self.player_1[1]}\n\n{self.player_2[1]} {self.player_2[0]}")
+            print(f"{self.player_1[1]} {self.player_1[0]}\n\n{self.player_2[0]} {self.player_2[1]}")
 
     def input_(self):
         while True:
@@ -35,6 +35,14 @@ class Waribashi:
             self.player_2[enemy] += self.player_1[me]
         else:
             self.player_1[enemy] += self.player_2[me]
+        if self.player_1[0] >= 5:
+            self.player_1[0] += -5
+        elif self.player_1[1] >= 5:
+            self.player_1[1] += -5
+        elif self.player_2[0] >= 5:
+            self.player_2[0] += -5
+        elif self.player_2[1] >= 5:
+            self.player_2[1] += -5
 
     def trial(self):
         if self.player_1 == [5, 5]:
