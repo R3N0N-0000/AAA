b10eb63141d52bafc83a4f8bc40bd764e6371d84
R3N0N-0000 <117981827+R3N0N-0000@users.noreply.github.com>
2022-12-27 06:42:12 +0000

    :(

--- すごろく.py
+++ すごろく.py
@@ -1,2 +1,22 @@
 import random
 
+num = 15
+
+while num > 0:
+    dice = random.randint(1, 6)
+    num -= dice
+    while True:
+        question = input("サイコロを振りますか？ y/n\n")
+        if question == "y":
+            break_ = False
+            break
+        elif question == "n":
+            break_ = True
+            break
+    if break_:
+        break
+    print("\nサイコロの目は", str(dice), "です")
+    if num > 0:
+        print("ゴールまであと", str(num),"\n")
+    else:
+        print("GOAL")
